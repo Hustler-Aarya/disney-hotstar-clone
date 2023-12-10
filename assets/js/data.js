a6e574394fd490f99d61242828b89edbb0e6a8b7
Aarya <aaryakapoor10@gmail.com>
2023-12-11 01:15:08 +0530

    first commit

---
+++ assets/js/data.js
@@ -0,0 +1,27 @@
+let movies = [
+  {
+    name: "wanda vision",
+    des: "!do watch it you will love it",
+    image: "assets/img/slider 3.png",
+  },
+  {
+    name: "falcon and the winter soldier",
+    des: "this movie is must watch",
+    image: "assets/img/slider 2.png",
+  },
+  {
+    name: "raya and the last dragon",
+    des: "this movie is must watch",
+    image: "assets/img/slider 4.png",
+  },
+  {
+    name: "loki",
+    des: "this movie is must watch",
+    image: "assets/img/slider 1.png",
+  },
+  {
+    name: "luca",
+    des: "this movie is must watch",
+    image: "assets/img/slider 5.png",
+  },
+];
